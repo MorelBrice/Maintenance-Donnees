e640f4d05673dac100530ae31effdb53dad5a812
B.Morel <brice.morel1@gmail.com>
2023-03-06 21:22:42 -0500

    menu principal fonctionnel

--- ConnexionMySQL.java
+++ ConnexionMySQL.java
@@ -3,12 +3,16 @@ import java.sql.*;
 public class ConnexionMySQL {
 	Connection mysql=null;
     boolean connecte=false;
-	String serveur = "jdbc:mysql://localhost:3306/SAE_PONEY";
+	String serveur = "jdbc:mysql://localhost:3306/SAE_PONEY?useSSL=true&serverTimezone=CET";
     
 	public ConnexionMySQL(String nomServeur, String nomBase, String nomLogin, String motDePasse) {
 		try {
+			// mysql = DriverManager.getConnection(
+			// 		"jdbc:mysql://"+nomServeur+":3306/"+nomBase, nomLogin, motDePasse);
+
 			mysql = DriverManager.getConnection(
-					"jdbc:mysql://"+nomServeur+":3306/"+nomBase, nomLogin, motDePasse);
+				serveur, nomLogin, motDePasse);
+
 			connecte=true;
 		} catch (SQLException e) {
 			System.out.println("Echec de connexion!"); 
--- ExePoney.java
+++ ExePoney.java
@@ -1,37 +1,95 @@
 import java.sql.SQLException;
 import java.util.Scanner;
 
-// commande à executer :
-
-//java -cp .:/usr/share/java/mariadb-java-client.jar TestJDBC
-// le serveur est "servinfo-mariadb"
-
-
 public class ExePoney {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static boolean finiPrincipal = false;
+    public static boolean finiInsc = false;
+    public static boolean finiPoney = false;
+    public static boolean finiPersonne = false;
+    public static boolean finiEleve = false;
+    public static boolean finiMoniteur = false;
+    public static boolean finiCours = false;
+
+    public static String nomServer = "localhost";
+    public static String base = "SAE_PONEY";
+    public static String login;
+    public static String mdp;
+
+    public static ConnexionMySQL c;
+
     public static void main(String[] args) throws SQLException {
-        Scanner sc = new Scanner(System.in);
 
         // On demande les info de la base de donneées
-        System.out.print("Veuillez donner le nom du serveur : ");
-        String nomServer = sc.nextLine();
-        System.out.print("Le nom de la base choisit : ");
-        String base = sc.nextLine();
         System.out.print("Votre login : ");
-        String login = sc.nextLine();
+        login = sc.nextLine();
         System.out.print("Votre mot de passe : ");
-        String mdp = sc.nextLine();
+        mdp = sc.nextLine();
 
         //Connection a la base de données
-        ConnexionMySQL c = new ConnexionMySQL(nomServer, base, login, mdp);
-        boolean fini = false;
-        while(!fini){
+        c = new ConnexionMySQL(nomServer, base, login, mdp);
+        
+
+        while(!finiPrincipal){
             System.out.println(" _____________________________");
-            System.out.println("|     Que souhaiter vous ?    |");
+            System.out.println("|     Que souhaitez vous ?    |");
+            System.out.println("|-----------------------------|");
+            System.out.println("| 1. Gérer les Inscriptions   |");
+            System.out.println("| 2. Gérer les Poneys         |");
+            System.out.println("| 3. Gérer les Personnes      |");
+            System.out.println("| 4. Gérer les élèves         |");
+            System.out.println("| 5. Gérer les Moniteurs      |");
+            System.out.println("| 6. Gérer les Cours          |");
+            System.out.println("| 7. Quitter                  |");
+            System.out.println("|_____________________________|");
+            System.out.print("\n");
+            int rep = sc.nextInt();
+            switch(rep){
+                case 1 :
+                    gererInscriptions();
+                    break;
+
+                case 2 :
+                    gererPoneys();
+                    break;
+
+                case 3:
+                    gererPersonnes();
+                    break;
+
+                case 4:
+                    gererEleves();
+                    break;
+
+                case 5:
+                    gererMoniteurs();
+                    break;
+
+                case 6:
+                    gererCours();
+                    break;
+
+                default : 
+                    finiPrincipal = true;
+                    sc.close();
+                    break;
+            }
+        }
+    }
+
+
+    public static void gererInscriptions() throws SQLException{
+
+        while(!finiInsc){
+            System.out.println(" _____________________________");
+            System.out.println("|     Que souhaitez vous ?    |");
             System.out.println("|-----------------------------|");
             System.out.println("| 1. Ajouter une inscription  |");
             System.out.println("| 2. Enlever une inscription  |");
             System.out.println("| 3. Regarder les cours       |");
-            System.out.println("| 4. Quitter                  |");
+            System.out.println("| 4. Retour                   |");
             System.out.println("|_____________________________|");
             System.out.print("\n");
             int rep = sc.nextInt();
@@ -76,10 +134,181 @@ public class ExePoney {
                     break;
 
                 default : 
-                    fini = true;
-                    sc.close();
+                    finiInsc = true;
                     break;
             }
         }
     }
+
+    public static void gererPoneys() throws SQLException{
+
+        while(!finiPoney){
+            System.out.println(" _____________________________");
+            System.out.println("|     Que souhaitez vous ?    |");
+            System.out.println("|-----------------------------|");
+            System.out.println("| 1. Lister les Poneys        |");
+            System.out.println("| 2. Ajouter un Poney         |");
+            System.out.println("| 3. Supprimer un Poney       |");
+            System.out.println("| 4. Modifier un Poney        |");
+            System.out.println("| 5. Retour                   |");
+            System.out.println("|_____________________________|");
+            System.out.print("\n");
+            int rep = sc.nextInt();
+            switch(rep){
+                case 1 :
+                    break;
+
+                case 2 :
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default : 
+                    finiPoney = true;
+                    break;
+            }
+        }
+    }
+
+    public static void gererPersonnes() throws SQLException{
+
+        while(!finiPersonne){
+        System.out.println(" _____________________________");
+        System.out.println("|     Que souhaitez vous ?    |");
+        System.out.println("|-----------------------------|");
+        System.out.println("| 1. Lister les Personnes     |");
+        System.out.println("| 2. Ajouter une Personne     |");
+        System.out.println("| 3. Supprimer une Personne   |");
+        System.out.println("| 4. Modifier une Personne    |");
+        System.out.println("| 5. Retour                   |");
+        System.out.println("|_____________________________|");
+        System.out.print("\n");
+        int rep = sc.nextInt();
+        switch(rep){
+            case 1 :
+                break;
+
+            case 2 :
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default : 
+                finiPersonne = true;
+                break;
+        }
+    }
+}
+
+    public static void gererEleves() throws SQLException{
+
+            while(!finiEleve){
+            System.out.println(" _____________________________");
+            System.out.println("|     Que souhaitez vous ?    |");
+            System.out.println("|-----------------------------|");
+            System.out.println("| 1. Lister les Eleves        |");
+            System.out.println("| 2. Ajouter un Eleve         |");
+            System.out.println("| 3. Supprimer un Eleve       |");
+            System.out.println("| 4. Modifier un Eleve        |");
+            System.out.println("| 5. Retour                   |");
+            System.out.println("|_____________________________|");
+            System.out.print("\n");
+            int rep = sc.nextInt();
+            switch(rep){
+                case 1 :
+                    break;
+
+                case 2 :
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default : 
+                    finiEleve = true;
+                    break;
+            }
+        }
+    }
+
+    public static void gererMoniteurs() throws SQLException{
+
+        while(!finiMoniteur){
+            System.out.println(" _____________________________");
+            System.out.println("|     Que souhaitez vous ?    |");
+            System.out.println("|-----------------------------|");
+            System.out.println("| 1. Lister les Moniteurs     |");
+            System.out.println("| 2. Ajouter un Moniteur      |");
+            System.out.println("| 3. Supprimer un Moniteur    |");
+            System.out.println("| 4. Modifier un Moniteur     |");
+            System.out.println("| 5. Retour                   |");
+            System.out.println("|_____________________________|");
+            System.out.print("\n");
+            int rep = sc.nextInt();
+            switch(rep){
+                case 1 :
+                    break;
+
+                case 2 :
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default : 
+                    finiMoniteur = true;
+                    break;
+            }
+        }
+    }
+
+    public static void gererCours() throws SQLException{
+
+        while(!finiCours){
+            System.out.println(" _____________________________");
+            System.out.println("|     Que souhaitez vous ?    |");
+            System.out.println("|-----------------------------|");
+            System.out.println("| 1. Lister les Cours         |");
+            System.out.println("| 2. Ajouter un Cours         |");
+            System.out.println("| 3. Supprimer un Cours       |");
+            System.out.println("| 4. Modifier un Cours        |");
+            System.out.println("| 5. Retour                   |");
+            System.out.println("|_____________________________|");
+            System.out.print("\n");
+            int rep = sc.nextInt();
+            switch(rep){
+                case 1 :
+                    break;
+
+                case 2 :
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default : 
+                    finiCours = true;
+                    break;
+            }
+        }
+    }
+
+
 }
